84755f30c10d052fd219059047af82dfa76cb9e2
zue <zue@kakao.com>
2020-04-18 17:50:23 +0900

    Revert "[javascript] promise 사용"
    This reverts commit ba6abf7583fec6baf84569f2b325642b6f34767f.

--- javascript/Promise.js
+++ javascript/Promise.js
@@ -1,3 +1,5 @@
+import React from "./node_modules/react";
+
 function increase(n, callback) {
   setTimeout(() => {
     const print = n + 1;
@@ -8,7 +10,6 @@ function increase(n, callback) {
   }, 1000);
 }
 
-// 콜백지옥..
 increase(0, n => {
   increase(n, n => {
     increase(n, n => {
@@ -21,32 +22,8 @@ increase(0, n => {
   });
 });
 
-function usingPromise(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const val = n + 1;
-
-      // reject action
-      if (val === 5) {
-        const err = new Error();
-        err.name = "value is 5";
-        reject(err);
-        return;
-      }
-
-      // resolve action
-      console.log(val);
-      resolve(val);
-    }, 1000);
-  });
+function PromiseSample() {
+  return <div>난 콘솔만 보고싶어</div>;
 }
 
-usingPromise(0)
-  .then(usingPromise)
-  .then(usingPromise)
-  .then(usingPromise)
-  .then(usingPromise)
-  .then(usingPromise)
-  .catch(e => {
-    console.error(e);
-  });
+export default PromiseSample;
